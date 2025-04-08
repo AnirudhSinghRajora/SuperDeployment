7cc1759ca547546aa2d169e9c1488d3daeb061a7
Anirudh <anirudh.s.me@gmail.com>
2025-04-08 20:16:56 +0530

    Level2 done, many typos

--- Level 2/src/App.js
+++ Level 2/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import NQueen from './Compenents/Visualizer/nqueenComponent';
+import NQueen from './Compenents/Visualizer/nqueenComponent.jsx';
 
 function App() {
 	return (
 		<div className="App">
-			<NQuee />
+			<NQueen />
 		</div>
 	);
 }
